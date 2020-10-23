2b9c678b3750aa41885656f3dec859866fd5036f
alu@ubuntu20-ZeDHP <dev.bhorta@gmail.com>
2020-10-23 11:12:16 +0600

    initial commit

---
+++ src/ExamplePage.tsx
@@ -0,0 +1,31 @@
+import { Typography, Button, Box, IconButton } from "@material-ui/core";
+import React from "react";
+import {
+  PageContainer,
+  FixedTopBar,
+  FixedMiddleBodyWithVerticalScroll,
+  FixedBottomProminentButton,
+  TopbarBackButton
+} from "./layout-components";
+import _ from "lodash";
+
+const ExamplePage = () => {
+  const topbarLeftButton: TopbarBackButton = {
+    type: "back",
+    onClick: () => console.log("Clicked back")
+  };
+  return (
+    <PageContainer>
+      <FixedTopBar title="Example Page Title" leftButton={topbarLeftButton} />
+      <FixedMiddleBodyWithVerticalScroll>
+        {/* Body goes here */}
+      </FixedMiddleBodyWithVerticalScroll>
+      <FixedBottomProminentButton
+        title="Test / Debug"
+        onClick={() => console.log("TODO - whatever you want to test/debug")}
+      />
+    </PageContainer>
+  );
+};
+
+export default ExamplePage
